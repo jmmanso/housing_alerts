9c35f6f4cd2005d1870772a3cec39ad68bd5ad9a
jesus-martinez <martinez@Jesuss-MacBook-Pro.local>
2016-10-24 16:06:32 -0700

    improving relative paths

--- configs.py
+++ configs.py
@@ -1,13 +1,12 @@
-
+import os
 
 cl_search_url = "https://sfbay.craigslist.org/search/sfc/apa?hasPic=1&nh=4&nh=12&nh=18&max_price=2500&bedrooms=1&bathrooms=1&minSqft=500&availabilityMode=0"
 
+thisdir = os.path.dirname(__file__)
+cl_dump_json_path = thisdir+"/files/cl_historic.json"
+cl_new_json_path = thisdir+"/files/cl_new.json"
 
-cl_dump_json_path = "files/cl_historic.json"
-cl_new_json_path = "files/cl_new.json"
-
-
-email_uname = "alfonso.perez.munoz72@hotmail.com" 
+email_uname = "alfonso.perez.munoz72@hotmail.com"
 email_hostname = "smtp.live.com"
 email_port = 587 #25
 
