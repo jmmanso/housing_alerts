a7a646ac13aca02a8e990c1a771b21e53b74e046
jesus-martinez <martinez@Jesuss-MacBook-Pro.local>
2016-10-24 15:16:51 -0700

    adding scripts

--- findmyapt.py
+++ findmyapt.py
@@ -1,57 +1,63 @@
 
-
-import urllib2 
+import os
+import json
+import urllib2
 import xmltodict
 from bs4 import BeautifulSoup
 from bs4 import BeautifulStoneSoup
 import bs4
+####
+import configs
+import emailer
 
-url = 'https://sfbay.craigslist.org/search/sfc/apa?is_paid=all&hasPic=1&search_distance_type=mi&nh=4&nh=5&nh=8&nh=11&nh=12&nh=10&nh=20&nh=18&nh=19&nh=23&nh=27&nh=2&max_price=2500&bedrooms=1&bathrooms=1&minSqft=450&no_smoking=1'
 
 
-rawcontents = urllib2.urlopen(url).read()
-
+rawcontents = urllib2.urlopen(configs.cl_search_url).read()
 rawcontents = rawcontents.replace('class','clase')
-
-soup = BeautifulSoup(rawcontents)
-
-
-soup.findAll('span', clase='price')
-
-with open('pretty.html','w') as f:
-	f.write(soup.prettify().encode('utf-8'))
-
+soup = BeautifulSoup(rawcontents, "lxml")
 
 
 listings = soup.findAll('span', clase='txt')
-listings[0].findAll('span', clase='pl')
+#listings[0].findAll('span', clase='pl')
 
-topdic = {}
+todays_listings = {}
 for i,l in enumerate(listings):
-	dic = {}
-	dic['description'] = l.find('a', clase='hdrlnk').text
-	dic['href'] = 'http://sfbay.craigslist.org'+l.find('a', clase='hdrlnk')['href']
-	dic['datetime'] = l.find('time')['datetime']
+	dic_ = {}
+	dic_['description'] = l.find('a', clase='hdrlnk').text
+	post_id = l.find('a', clase='hdrlnk')['href']
+	dic_['href'] = "http://sfbay.craigslist.org" + post_id
+	dic_['datetime'] = l.find('time')['datetime']
 	price_ = l.find('span',clase='price').text
-	dic['price'] = int(price_.split('$')[1])
+	dic_['price'] = int(price_.split('$')[1])
 	size_ = l.find('span',clase='housing').text
-	dic['size'] = int(size_.split('ft')[0][-3:])
-	dic['location'] = l.find('small').text.replace('(','').replace(')','').strip()
-	topdic[i] = dic
+	dic_['size'] = int(size_.split('ft')[0][-3:])
+	dic_['location'] = l.find('small').text.replace('(','').replace(')','').strip()
+	todays_listings[post_id] = dic_
 
+new_listings = {}
 
-xml_ = listings[0]
+if not os.path.exists(configs.cl_dump_json_path):
+	print "creating new json"
+	with open(configs.cl_dump_json_path, "w") as f:
+		f.write(json.dumps(todays_listings))
+	#
+	new_listings = dict(todays_listings)
+else:
+	print "loading existing json"
+	with open(configs.cl_dump_json_path, "r") as f:
+		historic_listings = json.loads(f.read())
+		for key in todays_listings.keys():
+			if key not in historic_listings.keys():
+				historic_listings[key] = todays_listings[key]
+				new_listings[key] = todays_listings[key]
+	with open(configs.cl_dump_json_path, "w") as f:
+		f.write(json.dumps(historic_listings))
 
-u = BeautifulSoup(xml_, "xml")
+with open(configs.cl_new_json_path, "w") as f:
+	f.write(json.dumps(new_listings))
 
-xmltodict.parse(unicode(xml_))
-
-
-
-
-
-
-
-
-
-
+if len(new_listings)>0:
+	print "there are new listings..."
+	emailer.mail(new_listings)
+else:
+	print "NO new listings."
