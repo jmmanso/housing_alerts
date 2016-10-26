8f1c7706853c3dd9572d6c2aa1571ea7d1a7ffd8
jesus-martinez <martinez@Jesuss-MacBook-Pro.local>
2016-10-25 19:14:28 -0700

    adding dolores search

--- configs.py
+++ configs.py
@@ -1,6 +1,10 @@
 import os
 
-cl_search_url = "https://sfbay.craigslist.org/search/sfc/apa?hasPic=1&nh=4&nh=12&nh=18&max_price=2500&bedrooms=1&bathrooms=1&minSqft=500&availabilityMode=0"
+cl_search_url = "https://sfbay.craigslist.org/search/sfc/apa?hasPic=1&nh=4&nh=12&nh=18&max_price=2500&bedrooms=1&bathrooms=1&availabilityMode=0"
+
+dolores_search_url240 = "http://www.240doloresapts.com/floorplan/1x1/"
+dolores_search_url230 = "http://www.230doloresapts.com/floorplan/1x1/"
+
 
 thisdir = os.path.dirname(__file__)
 cl_dump_json_path = thisdir+"/files/cl_historic.json"
--- findmyapt.py
+++ findmyapt.py
@@ -27,10 +27,29 @@ for i,l in enumerate(listings):
 	dic_['datetime'] = l.find('time')['datetime']
 	price_ = l.find('span',clase='price').text
 	dic_['price'] = int(price_.split('$')[1])
-	size_ = l.find('span',clase='housing').text
-	dic_['size'] = int(size_.split('ft')[0][-3:])
+	try:
+		size_ = l.find('span',clase='housing').text
+		dic_['size'] = int(size_.split('ft')[0][-3:])
+	except:
+		dic_['size'] = 'n/a'
 	dic_['location'] = l.find('small').text.replace('(','').replace(')','').strip()
 	todays_listings[post_id] = dic_
+
+##
+for url in [configs.dolores_search_url230, configs.dolores_search_url240]:
+	rawcontents = urllib2.urlopen(url).read()
+	soup = BeautifulSoup(rawcontents, "lxml")
+	text_ = soup.text.lower()
+	if "apply now" in text_ and "available" in text_:
+		dic_ = {}
+		dic_['description'] = "DOLORES Apartments"
+		dic_['href'] = url
+		dic_['price'] = 'n/a'
+		dic_['size'] = 'n/a'
+		dic_['location'] = 'n/a'
+		todays_listings[url[11:14]] = dic_
+##
+
 
 new_listings = {}
 
@@ -53,6 +72,8 @@ else:
 
 with open(configs.cl_new_json_path, "w") as f:
 	f.write(json.dumps(new_listings))
+
+
 
 if len(new_listings)>0:
 	print "there are new listings..."
