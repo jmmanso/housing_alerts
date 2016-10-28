d1687376b04ed4ec3cda74877565127ee8c97412
jesus-martinez <martinez@Jesuss-MacBook-Pro.local>
2016-10-28 10:52:02 -0700

    moving email configs to env vars

--- configs.py
+++ configs.py
@@ -1,18 +1,24 @@
 import os
+thisdir = os.path.dirname(__file__)
 
+# Specify Craigslist search url
 cl_search_url = "https://sfbay.craigslist.org/search/sfc/apa?hasPic=1&nh=4&nh=12&nh=18&max_price=2500&bedrooms=1&bathrooms=1&availabilityMode=0"
 
+# Specify other urls
 dolores_search_url240 = "http://www.240doloresapts.com/floorplan/1x1/"
 dolores_search_url230 = "http://www.230doloresapts.com/floorplan/1x1/"
 
-
-thisdir = os.path.dirname(__file__)
+# Path to save results
 cl_dump_json_path = thisdir+"/files/cl_historic.json"
 cl_new_json_path = thisdir+"/files/cl_new.json"
 
-email_uname = "alfonso.perez.munoz72@hotmail.com"
-email_hostname = "smtp.live.com"
-email_port = 587 #25
-
-
-from_email = email_uname
+# Email information is retrieved from environment variables,
+# which you need to set beforehand
+try:
+    from_email_address = os.environ['FROM_EMAIL_ADDRESS']
+    from_email_smtpserver = os.environ['FROM_EMAIL_SMTPSERVER']
+    from_email_port = int(os.environ['FROM_EMAIL_PORT'])
+    from_email_pswd = os.environ['FROM_EMAIL_PSWD']
+    to_email_address = os.environ['TO_EMAIL_ADDRESS']
+except KeyError:
+    raise Exception("You need to set email configs as environment variables")
